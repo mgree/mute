1218d05025095a554b6771806f199b2717b8f632
Michael Greenberg <michael@greenberg.science>
2025-03-31 16:55:09 -0400

    first cut at autoconf build

--- lints/version.sh
+++ lints/version.sh
@@ -7,10 +7,12 @@ cd "$MUTE_TOP" || exit 1
 V_SCRIPT="$(grep 'MUTE_VERSION=' mute | cut -d'"' -f 2)"
 V_MANPAGE="$(grep 'MUTE(1)' man/mute.1.md | cut -d' ' -f 4)"
 V_README="$(grep -A 2 '## Version history' README.md | tail -n 1 | cut -d' ' -f 2)"
+V_CONFAC="v$(grep AC_INIT configure.ac | cut -d'[' -f3 | cut -d']' -f1)"
 
 echo "Checking versions match..."
-echo " Script: '$V_SCRIPT'"
-echo "Manpage: '$V_MANPAGE'"
-echo " README: '$V_README'"
+echo "      Script: '$V_SCRIPT'"
+echo "     Manpage: '$V_MANPAGE'"
+echo "      README: '$V_README'"
+echo "configure.ac: '$V_CONFAC'"
 
-[ "$V_SCRIPT" = "$V_MANPAGE" ] && [ "$V_SCRIPT" = "$V_README" ]
+[ "$V_SCRIPT" = "$V_MANPAGE" ] && [ "$V_SCRIPT" = "$V_README" ] && [ "$V_SCRIPT" = "$V_CONFAC" ]
